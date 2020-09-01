3f470fbf4032a38470e6f98d0e07f847100360c2
Arkadiusz Drukała <arkadiusz.drukala@gmail.com>
2020-09-01 13:12:20 +0200

    Updates

---
+++ precache-manifest.7639633f2dcadb4d04f4d6fd9a1d7d59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8323544660a095d41f32cc15e6a8a1c7",
+    "url": "/Doggos/index.html"
+  },
+  {
+    "revision": "8b8354f24a95ddbd6a76",
+    "url": "/Doggos/static/css/main.a68f3ca8.chunk.css"
+  },
+  {
+    "revision": "8787ce30e1f4b19d1b8b",
+    "url": "/Doggos/static/js/2.c3d0eed7.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/Doggos/static/js/2.c3d0eed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8354f24a95ddbd6a76",
+    "url": "/Doggos/static/js/main.de4a4a6a.chunk.js"
+  },
+  {
+    "revision": "949007f60ddcde41ee5a",
+    "url": "/Doggos/static/js/runtime-main.07d56dcf.js"
+  },
+  {
+    "revision": "6ff8b6da601037ae8dabca9e18418ec9",
+    "url": "/Doggos/static/media/logo.6ff8b6da.jpg"
+  }
+]);
